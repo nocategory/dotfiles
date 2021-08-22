e3b0516db3710c8b240e0b9f9ecd3dd395fcffe6
João <joao@joao.codes>
2021-08-22 19:14:50 +0100

    fix starship install command

--- install.sh
+++ install.sh
@@ -12,6 +12,5 @@ sudo apt-get install -y \
 
 sudo rm -rf $HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sh -c "$(curl -fsSL https://starship.rs/install.sh)" --yes
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 
