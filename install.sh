348ebfa1043b76e4137b8395baedca773444e802
João <joao@joao.codes>
2021-08-22 23:47:25 +0100

    try to clone repos again

--- install.sh
+++ install.sh
@@ -13,11 +13,12 @@ sudo apt-get install -y \
 
 sudo rm -rf $HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 
 cp -f .zshrc $HOME/.zshrc
 cp -f .config/starship.toml $HOME/.config/starship.toml
-
-cp -f ./plugins $HOME/.oh-my-zsh/custom
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 sudo chsh -s /usr/bin/zsh $USERNAME
