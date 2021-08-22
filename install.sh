7dbde0ab4fe9759a427099bb7bdbf3a796c59f54
João <joao@joao.codes>
2021-08-22 19:53:28 +0100

    fix command order

--- install.sh
+++ install.sh
@@ -10,10 +10,11 @@ sudo apt-get install -y \
   sudo \
   zsh
 
+
 sudo rm -rf $HOME/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp -f .zshrc $HOME/.zshrc
 cp -f .config/starship.toml $HOME/.config/starship.toml
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 sudo chsh -s /usr/bin/zsh $USERNAME
 
