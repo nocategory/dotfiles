063b5c96d7d5bad8898c01ae920b1899c8f2c138
João <joao@joao.codes>
2021-08-22 18:28:39 +0100

    sudo some commands in install script

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y \
   sudo \
   zsh
 
-rm -rf $HOME/.oh-my-zsh
+sudo rm -rf $HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" --yes
