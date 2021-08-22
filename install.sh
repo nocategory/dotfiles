f12d070c212b240f5bf8945be3c80152ac0cebe4
João <joao@joao.codes>
2021-08-22 21:10:24 +0100

    try to fix zsh syntax highlighting

--- install.sh
+++ install.sh
@@ -19,6 +19,8 @@ cp -f .config/starship.toml $HOME/.config/starship.toml
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 sudo chsh -s /usr/bin/zsh $USERNAME
